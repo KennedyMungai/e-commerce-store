c98c9eec76bab0a7d0146cab144b4f10399ce1d0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-27 14:53:46 +0300

    Updated the email confirmation link

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendVerificationMail = async (email: string, token: string) => {
-  const verificationLink = `${process.env.NEXT_PUBLIC_APP_URL}/verify-email?token=${token}`;
+  const verificationLink = `${process.env.NEXT_PUBLIC_APP_URL}/signup/verify-email?token=${token}`;
 
   await resend.emails.send({
     from: "onboarding@resend.dev",
