4b64c00b463778f5c49bda3ae9b31ea5e0485c41
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-27 12:48:40 +0300

    Created the verification queries lib file

---
+++ src/lib/verification-queries.ts
@@ -0,0 +1,45 @@
+import "server-only";
+
+import { db } from "@/db";
+import { verificationTokens } from "@/db/schema";
+import { VERIFICATION_TOKEN_EXPIRATION_IN_MINUTES } from "@/lib/constants";
+import { eq } from "drizzle-orm";
+
+export const getVerificationTokenByEmail = async (email: string) =>
+  await db
+    .select()
+    .from(verificationTokens)
+    .where(eq(verificationTokens.identifier, email))
+    .then((res) => res[0]);
+
+export const getVerificationTokenByToken = async (token: string) =>
+  await db
+    .select()
+    .from(verificationTokens)
+    .where(eq(verificationTokens.token, token))
+    .then((res) => res[0]);
+
+export const createVerificationToken = async (email: string) => {
+  const existingToken = await getVerificationTokenByEmail(email);
+  if (existingToken)
+    await db
+      .delete(verificationTokens)
+      .where(eq(verificationTokens.identifier, email));
+
+  const expires = new Date(
+    Date.now() + VERIFICATION_TOKEN_EXPIRATION_IN_MINUTES * 60 * 1000,
+  );
+
+  const token = Math.random().toString(36).substring(2, 9);
+
+  const [newVerificationToken] = await db
+    .insert(verificationTokens)
+    .values({
+      identifier: email,
+      token,
+      expires,
+    })
+    .returning({ token: verificationTokens.token });
+
+  return newVerificationToken;
+};
