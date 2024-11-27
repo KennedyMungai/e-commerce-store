df333abf5aee32a789c76879f16d2ed0a7083d67
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-27 14:50:18 +0300

    Defined the logic used to send the verification email

---
+++ src/lib/mail.ts
@@ -0,0 +1,14 @@
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendVerificationMail = async (email: string, token: string) => {
+  const verificationLink = `${process.env.NEXT_PUBLIC_APP_URL}/verify-email?token=${token}`;
+
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: email,
+    subject: "Confirm your email",
+    html: `<p>Click <a href="${verificationLink}">here</a> to verify your email</p>`,
+  });
+};
