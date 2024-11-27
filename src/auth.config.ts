613fb490843f7ac4227ff7069bae79c3038e85a4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-27 11:37:09 +0300

    Added some code to the Credentials provider

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -2,7 +2,37 @@ import { type NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import { SigninSchema } from "@/lib/validation";
+import { findUserByEmail } from "@/lib/user-queries";
+import bcrypt from "bcryptjs";
 
 export default {
-  providers: [GitHub, Google, Credentials],
+  providers: [
+    GitHub,
+    Google,
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = SigninSchema.parse(credentials);
+
+        const { email, password } = parsedCredentials;
+
+        const existingUser = await findUserByEmail(email);
+
+        if (!existingUser || !existingUser.password) return null;
+
+        const isPasswordValid = await bcrypt.compare(
+          password,
+          existingUser.password,
+        );
+
+        if (isPasswordValid) {
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          const { password: _, ...user } = existingUser;
+
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
