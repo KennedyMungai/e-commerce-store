537fbd753223f23fca2728ea82d3e3aa59c0b703
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-28 09:33:42 +0300

    Added some responsiveness to the auth layput

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -7,12 +7,12 @@ type Props = {
 
 const AuthLayout = ({ children }: Props) => {
   return (
-    <div className="grid h-full grid-cols-2">
-      <div className="flex flex-col items-center justify-center gap-4 bg-blue-300">
+    <div className="h-full lg:grid lg:grid-cols-2">
+      <div className="hidden flex-col items-center justify-center gap-4 bg-blue-300 lg:flex">
         <Image src="/logo.svg" alt="Logo" width={200} height={200} />
         <p className="text-3xl font-semibold text-white">E Commerce Store</p>
       </div>
-      <div className="flex items-center justify-center bg-white">
+      <div className="flex h-full items-center justify-center bg-white">
         {children}
       </div>
     </div>
