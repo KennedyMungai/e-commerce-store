4f611789ca1c22ebdcd533383c3a35d9da01be98
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-12-05 15:39:42 +0300

    Created the data table for categories

--- src/app/(admin)/dashboard/categories/page.tsx
+++ src/app/(admin)/dashboard/categories/page.tsx
@@ -1,5 +1,14 @@
+import { DataTable } from "@/components/data-table";
+import { CategoryColumns } from "@/features/categories/components/category-columns";
+
 const CategoriesPage = () => {
-  return <div>CategoriesPage</div>;
+  return (
+    <div className="h-full">
+      <div className="p-4">
+        <DataTable columns={CategoryColumns} data={[]} />
+      </div>
+    </div>
+  );
 };
 
 export default CategoriesPage;
---
+++ src/features/categories/components/category-columns.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { SelectCategoryType } from "@/db/schema";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDownIcon, MoreVerticalIcon } from "lucide-react";
+
+export const CategoryColumns: ColumnDef<SelectCategoryType>[] = [
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDownIcon className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "description",
+    header: "Description",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const { id } = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={"ghost"} className="p-0">
+              <MoreVerticalIcon className="size-6" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+            <DropdownMenuItem>Delete</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
