d28f90c341a44f8fc9329a714e2b76567a2d9590
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-12-05 10:47:54 +0300

    Added a logo and a search input field

--- src/components/store-navbar.tsx
+++ src/components/store-navbar.tsx
@@ -1,13 +1,26 @@
 "use client";
 
 import { ModeToggle } from "@/components/mode-toggle";
+import { Input } from "@/components/ui/input";
 import UserButton from "@/features/auth/components/user-button";
+import Image from "next/image";
+import Link from "next/link";
 
 const StoreNavbar = () => {
   return (
-    <nav className="flex h-14 items-center justify-between border-b p-2">
-      <div></div>
-      <div></div>
+    <nav className="flex h-14 items-center justify-between gap-4 border-b p-2">
+      <Link href="/">
+        <div className="flex items-center gap-2 text-neutral-600">
+          <Image src="/logo.svg" width={40} height={40} alt="Logo" />
+          <p className="text-lg font-semibold">E Commerce Store</p>
+        </div>
+      </Link>
+      <div className="flex-1 px-8">
+        <Input
+          className="w-full bg-slate-100 text-center"
+          placeholder="Search products"
+        />
+      </div>
       <div className="flex items-center gap-2">
         <ModeToggle />
         <UserButton />
