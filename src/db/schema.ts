d8026ad119497a8f1232bb060b803c16d251abfc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-12-05 12:59:02 +0300

    Created the e-commerce schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,8 @@
+import { relations, sql } from "drizzle-orm";
 import {
   boolean,
+  decimal,
+  geometry,
   integer,
   pgEnum,
   pgTable,
@@ -7,11 +10,19 @@ import {
   text,
   timestamp,
   uniqueIndex,
+  uuid,
   varchar,
 } from "drizzle-orm/pg-core";
 import type { AdapterAccountType } from "next-auth/adapters";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
 
-export const roleEnum = pgEnum("role", ["user", "admin", "superadmin"]);
+export const RoleEnum = pgEnum("role", ["user", "admin", "superadmin"]);
+
+export const ShippingMethods = pgEnum("shipping_method", [
+  "standard",
+  "express",
+]);
 
 export const users = pgTable(
   "user",
@@ -24,12 +35,18 @@ export const users = pgTable(
     emailVerified: timestamp("emailVerified", { mode: "date" }),
     image: text("image"),
     password: varchar("password", { length: 512 }),
-    role: roleEnum("role").default("user").notNull(),
+    role: RoleEnum("role").default("user").notNull(),
   },
   (table) => ({
     emailUniqueIndex: uniqueIndex("emailUniqueIndex").on(table.email),
   }),
 );
+
+export const UserRelations = relations(users, ({ many }) => ({
+  wishlists: many(WishList),
+  comments: many(Comment),
+  orders: many(Order),
+}));
 
 export const accounts = pgTable(
   "account",
@@ -113,3 +130,266 @@ export const PasswordResetToken = pgTable(
     };
   },
 );
+
+export const Product = pgTable("products", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: varchar("name", { length: 256 }).notNull(),
+  description: text("description").notNull(),
+  price: decimal("price").notNull(),
+  image_url: varchar("image_url", { length: 256 }).notNull(),
+  colors: text("colors")
+    .array()
+    .default(sql`ARRAY[]::text[]`),
+  sizes: text("colors")
+    .array()
+    .default(sql`ARRAY[]::text[]`),
+  supplier_id: uuid("supplier_id").references(() => Supplier.id, {
+    onDelete: "cascade",
+  }),
+  category_id: uuid("category_id").references(() => Category.id, {
+    onDelete: "cascade",
+  }),
+  quantity: integer("quantity").default(0).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
+});
+
+export const ProductRelations = relations(Product, ({ many, one }) => ({
+  supplier: one(Supplier, {
+    fields: [Product.supplier_id],
+    references: [Supplier.id],
+  }),
+  category: one(Category, {
+    fields: [Product.category_id],
+    references: [Category.id],
+  }),
+  comments: many(Comment),
+  wishlists: many(WishList),
+}));
+
+export const Category = pgTable("categories", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: varchar("name", { length: 256 }).notNull(),
+  description: varchar("description", { length: 256 }).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
+});
+
+export const CategoryRelations = relations(Category, ({ many }) => ({
+  products: many(Product),
+}));
+
+export const InsertCategorySchema = createInsertSchema(Category).omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+});
+
+export type InsertCategoryType = z.infer<typeof InsertCategorySchema>;
+
+export const SelectCategorySchema = createSelectSchema(Category);
+
+export type SelectCategoryType = z.infer<typeof SelectCategorySchema>;
+
+export const Order = pgTable("orders", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  user_id: text("user_id").references(() => users.id, { onDelete: "cascade" }),
+  location: geometry("location", { type: "point", mode: "xy", srid: 4326 }),
+  shippingMethod: ShippingMethods("shipping_method").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
+});
+
+export const OrderRelations = relations(Order, ({ one, many }) => ({
+  user: one(users, {
+    fields: [Order.user_id],
+    references: [users.id],
+  }),
+  orderItems: many(OrderItem),
+}));
+
+export const InsertOrderSchema = createInsertSchema(Order).omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+});
+
+export type InsertOrderType = z.infer<typeof InsertOrderSchema>;
+
+export const SelectOrderSchema = createSelectSchema(Order);
+
+export type SelectOrderType = z.infer<typeof SelectOrderSchema>;
+
+export const OrderItem = pgTable(
+  "order_items",
+  {
+    product_id: uuid("product_id")
+      .references(() => Product.id, { onDelete: "cascade" })
+      .notNull(),
+    order_id: uuid("order_id").references(() => Order.id, {
+      onDelete: "cascade",
+    }),
+    quantity: integer("quantity").default(1).notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.product_id, table.order_id] }),
+  }),
+);
+
+export const OrderItemRelations = relations(OrderItem, ({ one }) => ({
+  product: one(Product, {
+    fields: [OrderItem.product_id],
+    references: [Product.id],
+  }),
+  order: one(Order, {
+    fields: [OrderItem.order_id],
+    references: [Order.id],
+  }),
+}));
+
+export const InsertOrderItemSchema = createInsertSchema(OrderItem).omit({
+  createdAt: true,
+  updatedAt: true,
+});
+
+export type InsertOrderItemType = z.infer<typeof InsertOrderItemSchema>;
+
+export const SelectOrderItemSchema = createSelectSchema(OrderItem);
+
+export type SelectOrderItemType = z.infer<typeof SelectOrderItemSchema>;
+
+export const Comment = pgTable("comments", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  user_id: text("user_id").references(() => users.id, { onDelete: "cascade" }),
+  product_id: uuid("product_id").references(() => Product.id, {
+    onDelete: "cascade",
+  }),
+  commentText: text("comment").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
+});
+
+export const CommentRelations = relations(Comment, ({ one }) => ({
+  product: one(Product, {
+    fields: [Comment.product_id],
+    references: [Product.id],
+  }),
+  user: one(users, {
+    fields: [Comment.user_id],
+    references: [users.id],
+  }),
+}));
+
+export const InsertCommentSchema = createInsertSchema(Comment).omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+});
+
+export type InsertCommentType = z.infer<typeof InsertCommentSchema>;
+
+export const SelectCommentSchema = createSelectSchema(Comment);
+
+export type SelectCommentType = z.infer<typeof SelectCommentSchema>;
+
+export const ProductRating = pgTable("product_ratings", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  user_id: text("user_id").references(() => users.id, { onDelete: "cascade" }),
+  product_id: uuid("product_id").references(() => Product.id, {
+    onDelete: "cascade",
+  }),
+  rating: integer("rating").default(0).notNull(),
+});
+
+export const ProductRatingRelations = relations(ProductRating, ({ one }) => ({
+  product: one(Product, {
+    fields: [ProductRating.product_id],
+    references: [Product.id],
+  }),
+  user: one(users, {
+    fields: [ProductRating.user_id],
+    references: [users.id],
+  }),
+}));
+
+export const InsertProductRatingSchema = createInsertSchema(ProductRating).omit(
+  {
+    id: true,
+  },
+);
+
+export type InsertProductRatingType = z.infer<typeof InsertProductRatingSchema>;
+
+export const SelectProductRatingSchema = createSelectSchema(ProductRating);
+
+export type SelectProductRatingType = z.infer<typeof SelectProductRatingSchema>;
+
+export const Supplier = pgTable("suppliers", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: varchar("name", { length: 256 }).notNull(),
+  email: varchar("email", { length: 256 }).notNull(),
+  phoneNumber: varchar("phone_number", { length: 13 }).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
+});
+
+export const SupplierRelations = relations(Supplier, ({ many }) => ({
+  products: many(Product),
+}));
+
+export const InsertSupplierSchema = createInsertSchema(Supplier).omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+});
+
+export type InsertSupplierType = z.infer<typeof InsertSupplierSchema>;
+
+export const SelectSupplierSchema = createSelectSchema(Supplier);
+
+export type SelectSupplierType = z.infer<typeof SelectSupplierSchema>;
+
+export const WishList = pgTable(
+  "wishlists",
+  {
+    user_id: text("user_id").references(() => users.id, {
+      onDelete: "cascade",
+    }),
+    product_id: uuid("product_id").references(() => Product.id, {
+      onDelete: "cascade",
+    }),
+    created_at: timestamp("created_at").defaultNow().notNull(),
+    updated_at: timestamp("updated_at").$onUpdate(() => new Date()),
+  },
+  (table) => ({
+    pk: primaryKey({
+      columns: [table.user_id, table.product_id],
+    }),
+  }),
+);
+
+export const WishListRelations = relations(WishList, ({ one }) => ({
+  product: one(Product, {
+    fields: [WishList.product_id],
+    references: [Product.id],
+  }),
+  user: one(users, {
+    fields: [WishList.user_id],
+    references: [users.id],
+  }),
+}));
+
+export const InsertWishListSchema = createInsertSchema(WishList).omit({
+  created_at: true,
+  updated_at: true,
+});
+
+export type InsertWishListType = z.infer<typeof InsertWishListSchema>;
+
+export const SelectWishListSchema = createSelectSchema(WishList);
+
+export type SelectWishListType = z.infer<typeof SelectWishListSchema>;
+
+// TODO: Add a table for payment
