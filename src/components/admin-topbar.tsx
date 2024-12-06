1d412d0cd6fa378aa1a6ff1fc2a97afa4bd104f5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-12-06 11:13:55 +0300

    Fixed a dumb issue

--- src/components/admin-topbar.tsx
+++ src/components/admin-topbar.tsx
@@ -5,11 +5,22 @@ import { Button } from "@/components/ui/button";
 import { useAddCategoryDialog } from "@/features/categories/hooks/use-add-category-dialog";
 import { PlusIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const AdminTopbar = () => {
   const pathname = usePathname();
 
-  const { open: openAddCategory } = useAddCategoryDialog();
+  const { setIsOpen } = useAddCategoryDialog();
+
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => setIsMounted(true), []);
+
+  if (!isMounted) return null;
+
+  const handleClick = () => {
+    setIsOpen(true);
+  };
 
   return (
     <div className="flex h-14 w-full items-center justify-between border-b p-4">
@@ -18,8 +29,8 @@ const AdminTopbar = () => {
       </h3>
       <div className="flex items-center gap-4">
         {pathname !== "/dashboard" && (
-          <Button variant={"outline"} size="icon">
-            <PlusIcon className="size-5" onClick={() => openAddCategory()} />
+          <Button variant={"outline"} size="icon" onClick={handleClick}>
+            <PlusIcon className="size-5" />
           </Button>
         )}
         <ModeToggle />
