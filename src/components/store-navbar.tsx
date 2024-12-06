61933cbefb9034a491893de0d5b4157b7ed70933
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-12-06 09:59:31 +0300

    Added a button to go to the admin dashboard

--- src/components/store-navbar.tsx
+++ src/components/store-navbar.tsx
@@ -3,20 +3,27 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { Input } from "@/components/ui/input";
 import UserButton from "@/features/auth/components/user-button";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { HeadsetIcon } from "lucide-react";
 
 const StoreNavbar = () => {
+  const session = useSession();
+
   return (
     <nav className="h-14 border-b">
       <div className="mx-auto flex max-w-7xl items-center justify-between gap-4 p-2">
         <Link href="/">
           <div className="flex items-center gap-2 text-neutral-600">
             <Image src="/logo.svg" width={40} height={40} alt="Logo" />
-            <p className="text-lg font-semibold">E Commerce Store</p>
+            <p className="hidden text-lg font-semibold md:flex">
+              E Commerce Store
+            </p>
           </div>
         </Link>
-        <div className="flex-1 px-8">
+        <div className="hidden flex-1 px-8 md:flex">
           <Input
             className="w-full bg-slate-100 text-center"
             placeholder="Search products"
@@ -25,6 +32,15 @@ const StoreNavbar = () => {
         <div className="flex items-center gap-2">
           <ModeToggle />
           <UserButton />
+          {/* {session.data &&
+            (session.data.user?.role === "admin" ||
+              session.data.user?.role === "superadmin") && ( */}
+          <Button variant={"outline"} size="icon" asChild>
+            <Link href="/dashboard">
+              <HeadsetIcon className="size-5" />
+            </Link>
+          </Button>
+          {/* )} */}
         </div>
       </div>
     </nav>
