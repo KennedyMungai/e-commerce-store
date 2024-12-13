c0e66ba42e0a13159be6c9510e879692bde5c512
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-12-13 15:06:00 +0300

    Using the data fetched from the backend to fill the orders table

--- src/features/orders/components/orders-table.tsx
+++ src/features/orders/components/orders-table.tsx
@@ -1,12 +1,29 @@
 "use client";
 
 import { DataTable } from "@/components/data-table";
-import { OrdersColumns } from "./orders-columns";
+import { OrdersColumns } from "@/features/orders/components/orders-columns";
+import { useFetchOrders } from "@/features/orders/api/use-fetch-orders";
 
 const OrdersTable = () => {
+  const {
+    data: ordersData,
+    isPending: isOrdersPending,
+    isError: isOrdersError,
+  } = useFetchOrders();
+
+  if (isOrdersPending) return <div>Loading...</div>;
+
+  if (isOrdersError) return <div>Error</div>;
+
+  const orders = ordersData.data.map((order) => ({
+    ...order,
+    createdAt: new Date(order.createdAt),
+    updatedAt: order.updatedAt ? new Date(order.updatedAt) : null,
+  }));
+
   return (
     <div className="p-4">
-      <DataTable columns={OrdersColumns} data={[]} />
+      <DataTable columns={OrdersColumns} data={orders} />
     </div>
   );
 };
