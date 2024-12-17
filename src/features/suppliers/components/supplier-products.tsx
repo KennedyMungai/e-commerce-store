9b4259c6da73131430e7455da5a339d8c8a26976
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-12-17 10:17:34 +0300

    Made some type updates

--- src/features/suppliers/components/supplier-products.tsx
+++ src/features/suppliers/components/supplier-products.tsx
@@ -1,7 +1,19 @@
-import { SelectProductType } from "@/db/schema";
 import { ColumnDef } from "@tanstack/react-table";
 
-export const SupplierProductColumns: ColumnDef<SelectProductType>[] = [
+export const SupplierProductColumns: ColumnDef<{
+  createdAt: Date;
+  updatedAt: Date | null;
+  id: string;
+  name: string;
+  description: string;
+  price: string;
+  image_url: string | null;
+  colors: string[] | null;
+  sizes: string[] | null;
+  supplier_id: string;
+  category_id: string;
+  quantity: number;
+}>[] = [
   {
     accessorKey: "name",
     header: "Name",
