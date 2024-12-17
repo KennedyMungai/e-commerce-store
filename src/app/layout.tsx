5b67c874c1df1d168a56029bf2dfe6418f95844b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-12-17 12:11:03 +0300

    Uploadthing setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
+import { ourFileRouter } from "@/app/api/uploadthing/core";
 import "@/app/globals.css";
 import { auth } from "@/auth";
 import { Toaster } from "@/components/ui/sonner";
 import ClientProviders from "@/providers/client-providers";
 import QueryProvider from "@/providers/query-provider";
 import ThemeProvider from "@/providers/theme-provider";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import type { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
 import localFont from "next/font/local";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { extractRouterConfig } from "uploadthing/server";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -44,6 +47,9 @@ export default async function RootLayout({
           <ThemeProvider>
             <QueryProvider>
               <NuqsAdapter>
+                <NextSSRPlugin
+                  routerConfig={extractRouterConfig(ourFileRouter)}
+                />
                 {children}
                 <ClientProviders />
               </NuqsAdapter>
