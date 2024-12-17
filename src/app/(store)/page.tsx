de9ba301a38f926f8e06b2b103aeb107ccc65875
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-12-17 15:38:54 +0300

    Created and rendered a placeholder navigation menu component

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,7 +1,11 @@
+import NavigationMenuBar from "@/features/categories/components/navigation-menu-bar";
+
 const HomePage = () => {
   return (
     <main className="h-[91.5vh] bg-slate-50">
-      <div className="container mx-auto h-full overflow-y-auto bg-white p-2"></div>
+      <div className="container mx-auto h-full overflow-y-auto bg-white p-2">
+        <NavigationMenuBar />
+      </div>
     </main>
   );
 };
