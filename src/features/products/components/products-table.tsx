f5b87dd6e9ed48618d72e9d17730a6acf2cf1cb4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-12-17 10:36:40 +0300

    Rendering the products data

--- src/features/products/components/products-table.tsx
+++ src/features/products/components/products-table.tsx
@@ -1,12 +1,33 @@
 "use client";
 
 import { DataTable } from "@/components/data-table";
+import { useFetchProducts } from "@/features/products/api/use-fetch-products";
 import { ProductsColumns } from "@/features/products/components/products-columns";
 
-const ProductsTable = () => {
+type Props = {
+  id: string;
+};
+
+const ProductsTable = ({ id }: Props) => {
+  const {
+    data: productsData,
+    isPending: isProductsDataPending,
+    isError: isProductsDataError,
+  } = useFetchProducts(id);
+
+  if (isProductsDataPending) return <div>Loading...</div>;
+
+  if (isProductsDataError) return <div>Error</div>;
+
+  const products = productsData.data.map((product) => ({
+    ...product,
+    createdAt: new Date(product.createdAt),
+    updatedAt: product.updatedAt ? new Date(product.updatedAt) : null,
+  }));
+
   return (
     <div className="p-4">
-      <DataTable columns={ProductsColumns} data={[]} />
+      <DataTable columns={ProductsColumns} data={products} />
     </div>
   );
 };
