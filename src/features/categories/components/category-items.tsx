26569ac8b5ebc8581d652e380bbc84fd11f4ee43
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-12-18 15:45:45 +0300

    Rendering the products in cards inside the category items component

--- src/features/categories/components/category-items.tsx
+++ src/features/categories/components/category-items.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useFetchCategory } from "@/features/categories/api/use-fetch-category";
+import { useFetchProducts } from "@/features/products/api/use-fetch-products";
+import ProductCard from "@/features/products/components/product-card";
 
 type Props = {
   categoryId: string;
@@ -13,11 +15,36 @@ const CategoryItems = ({ categoryId }: Props) => {
     isError: isCategoryDataError,
   } = useFetchCategory(categoryId);
 
+  const {
+    data: productsData,
+    isPending: isProductsDataPending,
+    isError: isProductsDataError,
+  } = useFetchProducts(categoryId);
+
   // TODO: Update the loading markup
-  if (isCategoryDataPending) return <div>Loading...</div>;
+  if (isCategoryDataPending || isProductsDataPending)
+    return <div>Loading...</div>;
 
   // TODO: Update the error markup
-  if (isCategoryDataError) return <div>Error</div>;
+  if (isCategoryDataError || isProductsDataError) return <div>Error</div>;
+
+  const products = productsData.data
+    .map((product) => ({
+      ...product,
+      colors: product.colors as string[],
+      sizes: product.sizes as (
+        | "XS"
+        | "S"
+        | "M"
+        | "L"
+        | "XL"
+        | "XXL"
+        | "XXXL"
+      )[],
+      createdAt: new Date(product.createdAt),
+      updatedAt: product.updatedAt ? new Date(product.updatedAt) : null,
+    }))
+    .filter((product) => product.image_url !== null);
 
   return (
     <div className="h-full overflow-y-auto p-4">
@@ -27,6 +54,12 @@ const CategoryItems = ({ categoryId }: Props) => {
       <p className="text-sm text-muted-foreground">
         {categoryData?.data.description}
       </p>
+
+      <div className="mt-4 grid grid-cols-3 gap-4">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/features/products/components/product-card.tsx
@@ -0,0 +1,35 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { SelectProductType } from "@/db/schema";
+import Image from "next/image";
+import Link from "next/link";
+
+type Props = {
+  product: SelectProductType;
+};
+
+// TODO: Update the card markup and styling
+const ProductCard = ({ product }: Props) => {
+  return (
+    <Link href="/">
+      <Card className="h-96 w-64 rounded-sm bg-white">
+        <CardContent className="px-0 pb-2">
+          <Image
+            src={product.image_url ?? ""}
+            alt={product.name}
+            width={300}
+            height={300}
+            className="rounded-md object-contain"
+          />
+          <div>
+            <p className="text-center text-lg font-semibold">{product.name}</p>
+            <p className="text-center text-xs text-muted-foreground">
+              {product.name}
+            </p>
+          </div>
+        </CardContent>
+      </Card>
+    </Link>
+  );
+};
+
+export default ProductCard;
