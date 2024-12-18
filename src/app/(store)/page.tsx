4641f2528baf915b7eb338a3616d1cff7608657e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-12-18 12:54:32 +0300

    Removed the navigation menu component from the applicatioon

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,11 +1,7 @@
-import NavigationMenuBar from "@/features/categories/components/navigation-menu-bar";
-
 const HomePage = () => {
   return (
     <main className="h-[91.5vh] bg-slate-50">
-      <div className="container mx-auto h-full overflow-y-auto bg-white p-2">
-        <NavigationMenuBar />
-      </div>
+      <div className="container mx-auto h-full overflow-y-auto bg-white p-2"></div>
     </main>
   );
 };
