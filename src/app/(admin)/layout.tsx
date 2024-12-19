1db759452be1f6bea58c738dc3fb2f48a3f8d7da
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-12-19 10:36:28 +0300

    Redirecting a user that is not an admin or a superadmin from the dashboard homepage

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,12 +1,19 @@
+import { auth } from "@/auth";
 import AdminSidebar from "@/components/admin-sidebar";
 import AdminTopbar from "@/components/admin-topbar";
+import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
 type Props = {
   children: ReactNode;
 };
 
-const DashboardLayout = ({ children }: Props) => {
+const DashboardLayout = async ({ children }: Props) => {
+  const session = await auth();
+
+  if (session?.user?.role !== "superadmin" && session?.user?.role !== "admin")
+    redirect("/");
+
   return (
     <div className="flex h-full">
       <AdminSidebar />
