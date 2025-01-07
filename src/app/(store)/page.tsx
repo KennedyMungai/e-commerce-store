4f601f06d98a451535535560dd25249808b5ae7d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-01-07 15:36:20 +0300

    Gave the product slider a static hieight

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -7,8 +7,8 @@ import { ArrowDownIcon } from "lucide-react";
 
 const HomePage = () => {
   return (
-    <main className="scrollbar-none h-full overflow-y-auto">
-      <div className="flex h-[75vh] w-full justify-between gap-2">
+    <main className="h-full overflow-y-auto scrollbar-none">
+      <div className="mb-20 flex h-[75vh] w-full justify-between gap-2">
         <div>
           <CategoriesSidebar />
         </div>
@@ -17,13 +17,13 @@ const HomePage = () => {
           <BannersSection />
         </div>
       </div>
-      <Separator className="relative my-6">
+      <Separator className="relative my-8">
         <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 items-center gap-2 rounded-full border bg-white px-4 py-2 text-xs text-muted-foreground dark:bg-slate-800">
           <span className="animate-pulse">Browse our products</span>
           <ArrowDownIcon className="size-3 animate-bounce" />
         </div>
       </Separator>
-      <div>
+      <div className="max-h-72">
         <ProductsSlider />
       </div>
     </main>
